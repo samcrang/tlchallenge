692b79341e4b5df414718f333b9cb7d88eaac9e4
Sam Crang <sam.crang@gmail.com>
2021-01-30 21:05:47 +0000

    Extract description and remove redundant whitespace

--- tests/unit/test_pokedex.py
+++ tests/unit/test_pokedex.py
@@ -4,6 +4,26 @@ from tlchallenge.pokedex import Pokedex, BASE_URL
 
 
 @responses.activate
+def test_strips_whitespace_from_pokemon_description():
+    responses.add(
+        responses.GET,
+        BASE_URL + "charizard",
+        json={
+            "name": "charizard",
+            "flavor_text_entries": [
+                {"flavor_text": "something \nsomething\u000csomething."}
+            ],
+        },
+        status=200,
+    )
+
+    pokedex = Pokedex()
+    charizard = pokedex.fetch_pokemon("charizard")
+
+    assert charizard.description == "something something something."
+
+
+@responses.activate
 def test_returns_name_for_a_pokemon(fake_charizard_response):
     responses.add(
         responses.GET, BASE_URL + "charizard", json=fake_charizard_response, status=200
--- tlchallenge/pokedex.py
+++ tlchallenge/pokedex.py
@@ -1,3 +1,4 @@
+import re
 import requests
 
 BASE_URL = "https://pokeapi.co/api/v2/pokemon-species/"
@@ -9,9 +10,11 @@ class Pokedex:
 
         description = response["flavor_text_entries"][0]["flavor_text"]
 
-        return Pokemon(response["name"])
+        cleaned_description = re.sub(re.compile(r'\s+'), ' ', description)
+        return Pokemon(response["name"], cleaned_description)
 
 
 class Pokemon:
-    def __init__(self, name):
+    def __init__(self, name, description):
         self.name = name
+        self.description = description
