750af152476c6111a026aebebe3472b07fbaa6f9
Sam Crang <sam.crang@gmail.com>
2021-01-30 19:17:00 +0000

    Remove the stubbed response for the acceptance test
    This was just here so that we'd have a reasonable chance at having a
    correct acceptance test implementation.

--- tlchallenge/app.py
+++ tlchallenge/app.py
@@ -6,9 +6,4 @@ app = Flask(__name__)
 
 @app.route("/pokemon/<name>")
 def shakespearify_pokemon_description(name):
-    return jsonify(
-        {
-            "name": "charizard",
-            "description": "Charizard flies 'round the sky in search of powerful opponents. 't breathes fire of such most wondrous heat yond 't melts aught. However, 't nev'r turns its fiery breath on any opponent weaker than itself.",
-        }
-    )
+    pass
