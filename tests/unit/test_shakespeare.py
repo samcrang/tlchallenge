eaba21eafbff159634b3feafaa9b579d9538842c
Sam Crang <sam.crang@gmail.com>
2021-01-31 12:03:51 +0000

    Raise exception if Shakespeare API returns errors

--- tests/unit/test_shakespeare.py
+++ tests/unit/test_shakespeare.py
@@ -1,7 +1,7 @@
 import pytest
 import responses
 
-from tlchallenge.shakespeare import Shakespeare, URL
+from tlchallenge.shakespeare import Shakespeare, ShakespeareAPIError, URL
 
 
 @responses.activate
@@ -21,3 +21,17 @@ def test_translates_given_text_to_shakespeare(fake_charizard_translation_respons
         client.translate(description)
         == "Charizard flies 'round the sky in search of powerful opponents. 't breathes fire of such most wondrous heat yond 't melts aught. However,  't nev'r turns its fiery breath on any opponent weaker than itself."
     )
+
+
+@responses.activate
+@pytest.mark.parametrize("status", [400, 401, 500, 501])
+def test_throws_if_shakespeare_api_returns_non_successful_response(status):
+    responses.add(
+        responses.POST,
+        URL,
+        match=[responses.urlencoded_params_matcher({"text": "foo"})],
+        status=status,
+    )
+
+    with pytest.raises(ShakespeareAPIError):
+        Shakespeare().translate("foo")
--- tlchallenge/shakespeare.py
+++ tlchallenge/shakespeare.py
@@ -7,4 +7,11 @@ class Shakespeare:
     def translate(self, text):
         response = requests.post(URL, data={"text": text})
 
+        if response.status_code >= 400:
+            raise ShakespeareAPIError()
+
         return response.json()["contents"]["translated"]
+
+
+class ShakespeareAPIError(Exception):
+    pass
